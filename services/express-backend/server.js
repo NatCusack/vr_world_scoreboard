8039a454a8adb0c1321166fbd862daba515a78ee
James <curley633@hotmail.com>
2020-03-10 22:09:19 +0000

    Express connects to DB and reads Game_Tables. Mongo credentials stored locally in env file

--- services/express-backend/server.js
+++ services/express-backend/server.js
@@ -1,20 +1,36 @@
+// const db = require('./queries')
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+require('dotenv/config')
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 const PORT = 4000;
+require('nodemon')
+
+const url = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + process.env.CLUSTER_NAME + "/test"
+// const url = "mongodb+srv://VrDb:KitchenCompanion5@mongcluster-s1yci.mongodb.net/test";
+var db
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// mongoose.connect('mongodb://127.0.0.1.27017/todos', {
-//     useNewUrlParser: true });
-// const connection = mongoose.connection;
-
-// connection.once('open', function() {
-//     console.log("MongoDB database connection established Successfully")
-// })
-app.listen(PORT, function() {
+MongoClient.connect(url, (err, client) => {
+  if(err) return console.log(err)
+    db = client.db('Vr_World_Scoreboard')
+  app.listen(PORT, () => {
     console.log("Server is running on Port: " + PORT);
-});
+  });
+})
+
+app.get('/', (req, res) => {
+  var cursor = db.collection('Game_Tables').find()
+  db.collection('Game_Tables').find().toArray(function(err, results) {
+    console.log(results)
+  })
+})
+
+app.post('/quote', (req, res) => {
+  console.log('Hellooooooooooooooooo!')
+})
