4d0bfbdce0084a45e41c21f72e96e1e555537c53
NatCusack <G0338306@gmit.ie>
2020-03-24 18:22:16 +0000

    Adding About Pages and Instructions

--- services/react-frontend/src/app/index.js
+++ services/react-frontend/src/app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { NavBar } from '../components';
-import { SuperhotScores, HighscoresInsert, HighscoresUpdate, BeatSaber, ArizonaSunshineScores } from '../pages';
+import { SuperhotScores, HighscoresInsert, HighscoresUpdate, BeatSaber, ArizonaSunshineScores, BeatSaberAbout, SuperhotAbout, ArizonaSunshineAbout, PistolWhipAbout, Instructions } from '../pages';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -14,6 +14,11 @@ function App() {
         <Route path="/highscores/arizonasunshine" exact component={ArizonaSunshineScores} />
         <Route path="/highscores/create" exact component={HighscoresInsert} />
         <Route path="/highscores/update/:id" exact component={HighscoresUpdate} />
+        <Route path="/about/beatsaber" exact component={BeatSaberAbout} />
+        <Route path="/about/superhot" exact component={SuperhotAbout}/>
+        <Route path="/about/arizonasunshine" exact component={ArizonaSunshineAbout}/>
+        <Route path="/about/pistolwhip" exact component={PistolWhipAbout}/>
+        <Route path="/instructions" exact component={Instructions}/>
       </Switch>
     </Router>
   );
--- services/react-frontend/src/pages/index.js
+++ services/react-frontend/src/pages/index.js
@@ -3,5 +3,12 @@ import HighscoresInsert from './HighscoresInsert'
 import HighscoresUpdate from './HighscoresUpdate'
 import BeatSaber from './BeatSaber'
 import ArizonaSunshineScores from './ArizonaSunshineScores'
+import SuperhotAbout from "./SuperhotAbout"
+import BeatSaberAbout from "./BeatSaberAbout"
+import ArizonaSunshineAbout from "./ArizonaSunshineAbout"
+import PistolWhipAbout from "./PistolWhipAbout"
+import Instructions from "./Instructions"
 
-export { SuperhotScores, HighscoresInsert, HighscoresUpdate, BeatSaber, ArizonaSunshineScores }
+
+
+export { SuperhotScores, HighscoresInsert, HighscoresUpdate, BeatSaber, ArizonaSunshineScores, SuperhotAbout, BeatSaberAbout, ArizonaSunshineAbout, PistolWhipAbout, Instructions}
